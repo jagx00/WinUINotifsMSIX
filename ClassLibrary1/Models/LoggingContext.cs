5066be56285b24cd14fc57449f128786736ec89f
John Goudy <jagx00@hotmail.com>
2025-10-20 12:44:44 +1100

    JAGLog using explicit config ref

---
+++ ClassLibrary1/Models/LoggingContext.cs
@@ -0,0 +1,57 @@
+﻿//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// https://docs.microsoft.com/en-us/ef/core/miscellaneous/configuring-dbcontext
+// https://docs.microsoft.com/en-us/ef/core/modeling/
+// https://docs.microsoft.com/en-us/ef/core/managing-schemas/
+// If you want your EF Core model to be the source of truth, use Migrations. As you make changes to your EF Core model, this approach incrementally applies the corresponding schema changes to your database 
+// so that it remains compatible with your EF Core model.
+
+// Use Reverse Engineering if you want your database schema to be the source of truth. This approach allows you to scaffold a DbContext and the entity type classes by reverse engineering your database schema into an EF Core model.
+// https://docs.microsoft.com/en-us/ef/core/managing-schemas/scaffolding
+// Reverse engineering is the process of scaffolding entity type classes and a DbContext class based on a database schema.
+// It can be performed using the Scaffold-DbContext command of the EF Core Package Manager Console (PMC) tools or the dotnet ef dbcontext scaffold command of the .NET Command-line Interface (CLI) tools.
+
+// https://xamlbrewer.wordpress.com/category/entity-framework/
+
+namespace JAGLog.Models
+{
+    //public class LoggingContext : DbContext
+    //{
+    //    public LoggingContext(DbContextOptions<LoggingContext> options)
+    //        : base(options)
+    //    { }
+    //    #region Required
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.Entity<SeriLog>()
+    //            .Property(b => b.Id)
+    //            .IsRequired();
+     
+    //        modelBuilder.Entity<SeriLog>(entity =>
+    //        {
+    //            entity.HasKey(c => new { c.Id });
+    //        });
+
+    //        modelBuilder.Entity<SeriLog>()
+    //        .Property(b => b.TimeStamp);
+
+
+    //        // https://entityframeworkcore.com/knowledge-base/48267925/invalid-object-name-error---entityframeworkcore-2-0
+    //        // https://stackoverflow.com/questions/8262590/entity-framework-code-first-fluent-api-adding-indexes-to-columns
+    //        // https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/fluent/types-and-properties
+    //        // The code first fluent API is most commonly accessed by overriding the OnModelCreating method on your derived DbContext. 
+
+    //        // 20200602 - added index
+    //        modelBuilder.Entity<SeriLog>().ToTable("SeriLog")
+    //                                      .HasIndex(s => new { s.TimeStamp }).IsClustered(false);
+
+    //    }
+    //    #endregion
+
+    //    public DbSet<SeriLog> SeriLogs { get; set; }
+    //}
+}
---
+++ ClassLibrary1/ViewModels/BindableBase.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+//using Windows.UI.Xaml.Data;
+//   // https://xamlbrewer.wordpress.com/2016/01/31/binding-a-slider-to-an-enumeration-in-uwp/
+
+namespace JAGLog
+{
+    /// <summary>
+    /// Implementation of <see cref="INotifyPropertyChanged"/> to simplify models.
+    /// </summary>
+    /// 
+    // An abstract class is a class in C# that can’t be instantiated. An abstract class can have both abstract methods and Concrete method. Concrete methods are those methods that have methods with their implementation.
+    // https://www.guru99.com/c-sharp-abstract-class.html
+    // An abstract class should have a minimum of one abstract method. ?? 
+    // Abstract class acts as a base class and is designed to be inherited by subclasses that either implement or either override its method. 
+
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Multicast event for property change notifications.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //string s1;
+
+        /// <summary>
+        /// Checks if a property already matches a desired value.  Sets the property and
+        /// notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to a property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers that
+        /// support CallerMemberName.</param>
+        /// <returns>True if the value was changed, false if the existing value matched the
+        /// desired value.</returns>
+        //protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        //{
+        //    if (object.Equals(storage, value)) return false;
+        //    // should be called SetField as cannot set a ppty this way!
+        //    // storage should be called field or destination too?
+        //    storage = value;
+        //    this.OnPropertyChanged(propertyName);
+        //    return true;
+        //}
+        // 20200408
+        // for when setting ppty
+        //   set { SetProperty(ref importance, value); } 
+        //  set { SetProperty(ref _ClickCount, value); } = setting a field
+        //  set { SetProperty2(storage => App.gDiagLevel = storage, value); } works
+        //protected bool SetProperty2<T>(Action<T> storage, T value, [CallerMemberName] String propertyName = null)
+        //{
+        //    // works but pointless as SetProperty will always return true so may as well just execute App.gDiagLevel = storage directly 
+        //    // rather than calling this function to do the same thing
+        //    T value2 = default;
+        //    bool result = SetProperty(ref value2,value);
+        //    if (result) storage(value2);
+        //    return result;
+
+        //    //if (object.Equals(storage, value)) return false;
+
+        //    //storage = value;
+        //    //this.OnPropertyChanged(propertyName);
+        //    //return true;
+        //}
+        //not wrkg yet = storage is input only and contains a value not the actual object I want to update
+        // https://stackoverflow.com/questions/1402803/passing-properties-by-reference-in-c-sharp
+        // in detail
+        // conclusion = do a variation of the LINQ Expression vern or Accessor of SO above
+        // but hardly worth it and may even take more work
+//        protected bool SetProperty3<T>(string storage, T value, [CallerMemberName] String propertyName = null)
+//        {
+//            //Action<T> storageA = storagep => storage = storagep;
+//            // https://stackoverflow.com/questions/3059420/accessing-properties-through-generic-type-parameter
+//var xx = typeof(T).GetProperty(storage);
+//            s1 = typeof(T).GetProperty(storage).ToString();
+//            //
+//            bool result = true;
+//            //T value2 = default;
+//            //bool result = SetProperty(ref value2, value);
+//            //if (result) storageA(value2);
+//            return result;
+
+//            //if (object.Equals(storage, value)) return false;
+
+//            //storage = value;
+//            //this.OnPropertyChanged(propertyName);
+//            //return true;
+//        }
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

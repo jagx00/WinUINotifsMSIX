1f7de4fc912447d9523482f07c4d93f38094bf69
John Goudy <jagx00@hotmail.com>
2025-10-20 11:40:47 +1100

    removed jstdlog - no nuget errors

--- WinUINotifsMSIX/App.xaml.cs
+++ WinUINotifsMSIX/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using JStdLog;
+//using JStdLog;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
